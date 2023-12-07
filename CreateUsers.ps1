6980141c7b30e75ef8fb2fbce679a81032daed79
MXFire1906 <85999078+ManuelDogbatse@users.noreply.github.com>
2023-12-07 16:07:15 +0000

    Updated department loop function

--- CreateUsers.ps1
+++ CreateUsers.ps1
@@ -1,31 +1,36 @@
 ﻿# Get template password for all users
 $passwordStr = Read-Host -Prompt "Enter the password for users"
-#$passwordStr   = "Password123!"
-# Read names and cities TXT files
+#$passwordStr   = "Password123!"    # Hard coded
+# Read names and departments TXT files
 $nameList = Get-Content .\names.txt
-$cityList = Get-Content .\cities.txt
+$depList = Get-Content .\departments.txt
 
 # Convert password string to secure string for AD user creation
 $passwordSecStr = ConvertTo-SecureString $passwordStr -AsPlainText -Force
 
-# Create an OU for each city, with 'Users' and 'Computers' sub-OUs
-foreach  ($city in $cityList){
-    New-ADOrganizationalUnit -Name $city -ProtectedFromAccidentalDeletion $false
-    New-ADOrganizationalUnit -Name "Users" -ProtectedFromAccidentalDeletion $false -Path "OU=$($city),$(([ADSI]'').distinguishedName)"
-    New-ADOrganizationalUnit -Name "Computers" -ProtectedFromAccidentalDeletion $false -Path "OU=$($city),$(([ADSI]'').distinguishedName)"
+Create an OU for each department, with 'Users' and 'Computers' sub-OUs
+foreach  ($dep in $depList){
+    New-ADOrganizationalUnit -Name $dep -ProtectedFromAccidentalDeletion $false
+    New-ADOrganizationalUnit -Name "Users" -ProtectedFromAccidentalDeletion $false -Path "OU=$($dep),$(([ADSI]'').distinguishedName)"
+    New-ADOrganizationalUnit -Name "Computers" -ProtectedFromAccidentalDeletion $false -Path "OU=$($dep),$(([ADSI]'').distinguishedName)"
 }
 
 # 'textInfo' for converting names to title case
 $textInfo = (Get-Culture).TextInfo
 $cnt = 0
-$n = $cityList.Count
+$n = $depList.Count
 
 # Create each user in names TXT file
 foreach ($name in $nameList) {
     # Split line into forename and surname, and change to title case
-    $fName, $sName = $nameList.Split(" ") | ForEach-Object {$textInfo.ToTitleCase($_)}
+    $fName, $sName = $name.Split(" ") | ForEach-Object {$textInfo.ToTitleCase($_)}
     $uName = "$($fName.Substring(0,1))$($sName)".ToLower()
-    Write-Host "Creating user $($fName) $($sName) ($($uName))" -BackgroundColor Black -ForegroundColor Cyan
+    # Print current user and current department
+    Write-Host "Creating user " -ForegroundColor Cyan -NoNewline
+    Write-Host "$($fName) $($sName) ($($uName)) " -ForegroundColor Yellow -NoNewline
+    Write-Host "in the " -ForegroundColor Cyan -NoNewline
+    Write-Host "$($depList[$cnt]) " -ForegroundColor Red -NoNewline
+    Write-Host "department" -ForegroundColor Cyan
     
     # Create new user
     New-AdUser -AccountPassword $passwordSecStr `
@@ -35,8 +40,15 @@ foreach ($name in $nameList) {
                -Name $username `
                -EmployeeID $username `
                -PasswordNeverExpires $true `
-               # Add users to one of the cities, cycles through each city every time a user is created
+               # Add users to one of the departments
                # 'ADSI' accesses the domain name of the domain for user creation
-               -Path "OU=Users,OU=$($cityList[$cnt]),$(([ADSI]'').distinguishedName)"
+               -Path "OU=Users,OU=$($depList[$cnt]),$(([ADSI]'').distinguishedName)"
                -Enabled $true
+    
+    # Change departments
+    $cnt++
+    if ($cnt -eq $n)
+    {
+        $cnt = 0
+    }
 }
