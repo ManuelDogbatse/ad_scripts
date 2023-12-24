091933f3095496fd88af362dba1fde3a470b98ab
MXFire1906 <85999078+ManuelDogbatse@users.noreply.github.com>
2023-12-24 17:42:04 +0000

    Converted list to set

--- remove_duplicates.py
+++ remove_duplicates.py
@@ -1,10 +1,15 @@
+"""Removes duplicates from names.txt"""
 import os
 
 path = os.getcwd()+"\\names.txt"
 print(path)
 
-with open(path, "r") as file:
-    names = file.read().split("\n")
+with open(path, "r", encoding="utf-8") as file:
+    names_list = file.read().split("\n")
 
-print(names[0])
-print(len(names))
+print(names_list[0])
+print(len(names_list))
+
+names_set = sorted(set(names_list))
+print(names_set)
+print(len(names_set))
