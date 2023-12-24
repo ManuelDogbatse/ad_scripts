34bbdaf3d282400f80d38a73fe4665b4871f0c83
MXFire1906 <85999078+ManuelDogbatse@users.noreply.github.com>
2023-12-24 17:30:15 +0000

    Added remove duplicates

---
+++ remove_duplicates.py
@@ -0,0 +1,12 @@
+import os
+
+print(os.getcwd())
+
+# path = "names.txt"
+
+# with open(path, "r") as file:
+#     lines = file.read()
+#     lines_list = lines.split("\n")
+
+# print(lines_list[0])
+# print(len(lines_list))
