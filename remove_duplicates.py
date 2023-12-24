b6adabc64e3295e8c9a4e160a83784c8093180b3
MXFire1906 <85999078+ManuelDogbatse@users.noreply.github.com>
2023-12-24 19:16:20 +0000

    Remove empty lines from txt file

--- remove_duplicates.py
+++ remove_duplicates.py
@@ -1,17 +1,32 @@
 """Removes duplicates from names.txt"""
 import os
 
+# Get absolute file path for text file
 path = os.getcwd()
 names_file = path + "\\names.txt"
 names_new_file = path + "\\names_new.txt"
 
+# Reads text file and saves lines to list
 with open(names_file, "r", encoding="utf-8") as file:
     print("Reading file\n")
-    names_list = file.read().split("\n")
+    names_str = file.read()
+    # Removes empty lines in txt file
+    # Stores each name as a string in a list
+    names_list = [line for line in names_str.split("\n") if line.strip()]
 
-for i, name in enumerate(names_list):
-    if name == "":
-        del names_list[i]
+# initials_set = {}
+
+# f_list = names_list[0].split()
+# print(f_list)
+
+# count = 0
+
+# for i, name in enumerate(names_list):
+#     if re.search("\n\W", name):
+#         # del names_list[i]
+#         count += 1
+
+# print(f"Count: {count}")
 
 names_set = sorted(set(names_list))
 print("Duplicates removed!")
