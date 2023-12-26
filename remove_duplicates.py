36f9d452863f96624e4ca8f51dd74b3afc9722ed
MXFire1906 <85999078+ManuelDogbatse@users.noreply.github.com>
2023-12-26 12:34:35 +0000

    Completed text file formatting

--- remove_duplicates.py
+++ remove_duplicates.py
@@ -8,50 +8,29 @@ names_new_file = path + "\\names_new.txt"
 
 # Reads text file and saves lines to list
 with open(names_file, "r", encoding="utf-8") as file:
-    print("Reading file\n")
+    print("Reading file")
     names_str = file.read()
     # Removes empty lines in txt file
     # Stores each name as a string in a list
     names_list = [line for line in names_str.split("\n") if line.strip()]
     usernames_list = [name[0].lower() + name.split()[1].lower() for name in names_list]
 
+print("Creating dictionary of full names and usernames")
 # Merge names and usernames together
 name_uname_list = zip(usernames_list, names_list)
 # Convert into a dictionary to remove duplicate usernames
 names_dict = dict(name_uname_list)
 
-print(names_dict)
-print(len(names_dict))
+print("Sorting dictionary in alphabetic order")
+names_dict = dict(sorted(names_dict.items()))
 
+print("Duplicates removed successfully")
+print(f"Original names list count: {len(names_list)}\nNew names list count: {len(names_dict)}")
 
-# print("Names and usernames:")
-# for i in range(10):
-#     print(f"{name_uname_list[i]}, {name_uname_list[i][1]}")
+# Write new list of names to text file
+with open(names_new_file, "w", encoding="utf-8") as file:
+    print("Writing to file")
+    names_str = "\n".join(names_dict.values())
+    file.write(names_str)
 
-# initials_set = set()
-
-# for name in names_list:
-#     initials_set.add(name[0].lower() + name.split()[1].lower())
-
-# print(f"Name count: {len(names_list)}\nUsername count: {len(initials_set)}")
-
-# count = 0
-
-# for i, name in enumerate(names_list):
-#     if re.search("\n\W", name):
-#         # del names_list[i]
-#         count += 1
-
-# print(f"Count: {count}")
-
-# names_set = sorted(set(names_list))
-# print("Duplicates removed!")
-# print(f"Original Name Count: {len(names_list)}")
-# print(f"New Name Count: {len(names_set)}")
-
-# with open(names_new_file, "w", encoding="utf-8") as file:
-#     print("Writing to file\n")
-#     names_str = "\n".join(names_set)
-#     file.write(names_str)
-
-# print("File write complete!")
+print("File write complete")
