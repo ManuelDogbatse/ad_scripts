f6f6798115000c09ac01ec8d97583719dd1dd1dc
MXFire1906 <85999078+ManuelDogbatse@users.noreply.github.com>
2023-12-27 21:48:24 +0000

    Added script to delete OUs during testing

---
+++ DeleteOUs.ps1
@@ -0,0 +1,9 @@
+﻿$depList = Get-Content .\departments.txt
+
+foreach ($dep in $depList) {
+    $splitStr = $dep.Split(",")
+    $depNames += $splitStr[0]
+    $depInitials += $splitStr[1]
+
+    Remove-ADOrganizationalUnit -Identity "OU=_$($depNames[-1]),$(([ADSI]'').distinguishedName)" -Recursive
+}
